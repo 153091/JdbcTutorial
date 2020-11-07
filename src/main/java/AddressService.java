5f72095df7c71dbd6e4442ad8b1e2c311cd77f88
18741847 <AOKulakov@sberbank.ru>
2020-11-08 00:16:12 +0300

    readById

---
+++ src/main/java/AddressService.java
@@ -0,0 +1,122 @@
+import bl.Util;
+import dao.AddressDAO;
+import entity.Address;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressService extends Util implements AddressDAO {
+
+    private Connection connection = getConnection();
+
+    @Override
+    public void add(Address address) throws SQLException {
+
+        PreparedStatement preparedStatement = null;
+
+        String sql = "INSERT INTO ADDRESS (ID, COUNTRY, CITY, STREET, POST_CODE) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, address.getId());
+            preparedStatement.setString(2, address.getCountry());
+            preparedStatement.setString(3, address.getCity());
+            preparedStatement.setString(4, address.getStreet());
+            preparedStatement.setString(5, address.getPostCode());
+
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Address> readAll() throws SQLException {
+        List<Address> addressList = new ArrayList<>();
+
+        String sql = "Select * from ADDRESS";
+
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Address address = new Address();
+                address.setId(resultSet.getLong("ID"));
+                address.setCountry(resultSet.getString("COUNTRY"));
+                address.setCity(resultSet.getString("CITY"));
+                address.setStreet(resultSet.getString("STREET"));
+                address.setPostCode(resultSet.getString("POST_CODE"));
+
+                addressList.add(address);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+        return addressList;
+    }
+
+    @Override
+    public Address readById(long id) throws SQLException {
+        String sql = "Select * from ADDRESS where ID =?";
+
+        PreparedStatement preparedStatement = null;
+
+        Address address = new Address();
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+                address.setId(resultSet.getLong("ID"));
+                address.setCountry(resultSet.getString("COUNTRY"));
+                address.setCity(resultSet.getString("CITY"));
+                address.setStreet(resultSet.getString("STREET"));
+                address.setPostCode(resultSet.getString("POST_CODE"));
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+    }
+
+    @Override
+    public void update(Address address) {
+
+    }
+
+    @Override
+    public void remove(Address address) {
+
+    }
+}
--- src/main/java/dao/AddressDAO.java
+++ src/main/java/dao/AddressDAO.java
@@ -2,17 +2,18 @@ package dao;
 
 import entity.Address;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface AddressDAO {
 
     //create
-    void add(Address address);
+    void add(Address address) throws SQLException;
 
     //read
-    List<Address> readAll();
+    List<Address> readAll() throws SQLException;
 
-    Address readById(long id);
+    Address readById(long id) throws SQLException;
 
     //update
     void update(Address address);
